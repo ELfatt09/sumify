267564d979b4e021afe22499421c9239eb1e6da1
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-10-26 08:08:21 +0700

    `Update Notification.jsx: changed div background color to white`

--- src/partials/Notification.jsx
+++ src/partials/Notification.jsx
@@ -9,7 +9,7 @@ function Notification() {
     return (
     <>
         { isThereNotification && (
-          <div className={`z-50 fixed shadow-md rounded-lg border-2 ${notificationType === 'success' ? 'border-green-300' : 'border-red-300'} p-3 mx-10 mt-5 right-5`}>{ notificationType === 'success' ? '✅ ' : '❌ ' }{notificationMessage}</div>
+          <div className={`z-50 fixed bg-white shadow-md rounded-lg border-2 ${notificationType === 'success' ? 'border-green-300' : 'border-red-300'} p-3 mx-10 mt-5 right-5`}>{ notificationType === 'success' ? '✅ ' : '❌ ' }{notificationMessage}</div>
         )
             }
     </>
